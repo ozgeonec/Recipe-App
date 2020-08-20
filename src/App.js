384e09b6dbbafa9529080ce5a4f5d269ca24a7b2
ozgeonec <ozgenuronec@gmail.com>
2020-08-20 18:49:19 +0300

    creating search bar and api requests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+    const APP_ID="63f9c44a"
+    const APP_KEY= "fb39bd52d9bee78117fdf01125967344"
+    const exampleReq= 'https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}'
+
+    useEffect(()=>{
+
+    })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form className="search-form">
+            <input className="search-bar" type="text"/>
+            <button className="search-button" type="submit">Search</button>
+        </form>
+
     </div>
   );
 }
